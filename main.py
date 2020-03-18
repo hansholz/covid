4a0556275d3166d6003625c44f6dc63b4f522e0f
hans <amoremiosa13@gmail.com>
2020-03-18 18:12:22 +0200

    implement searching for areas

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from telebot import types
 import sqlite3
 from response import kyiv_doctors
 
-
 with open("token.txt") as f:
     token = f.read().strip()
 TOKEN = f'{token}'
-
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -32,9 +30,27 @@ def choose_doctor(message):
     bot.send_message(message.chat.id, f'Which region you need? ', reply_markup=key)
     conn.close()
 
+
 @bot.message_handler(content_types=['text'])
-def kyiv_region(message):
-    # checking users answer
+def areas(message, region):
+    key = types.InlineKeyboardMarkup()
+
+    conn = sqlite3.connect('regions.sqlite3')
+    c = conn.cursor()
+    c.execute(f'SELECT region FROM regions WHERE id = {region};')
+    reg_centrum = list(c)[0][0]
+    c.execute(f'SELECT {reg_centrum.replace(" ", "_")} FROM areas')
+
+    data = list(c)
+    for name in data:
+        if name[0] is not None:
+            itembtn = types.InlineKeyboardButton(text=f"{name[0]}", callback_data=f"{name[0].replace(' ', '_')}")
+            key.add(itembtn)
+    bot.send_message(message.chat.id, f'Choose your area:', reply_markup=key)
+
+
+@bot.message_handler(content_types=['text'])
+def doctors_in_area(message):
     key = types.InlineKeyboardMarkup()
     for name in kyiv_doctors():
         itembtn = types.InlineKeyboardButton(text=f"{name}", callback_data=f"{name}")
@@ -45,10 +61,7 @@ def kyiv_region(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     # checking which button have been pressed
-    if call.data == "1":
-        bot.send_message(call.message.chat.id, f'Vinnytsya')
-    elif call.data == "9":
-        kyiv_region(call.message)
+    areas(call.message, f'{call.data}')
 
 
 bot.skip_pending = True
